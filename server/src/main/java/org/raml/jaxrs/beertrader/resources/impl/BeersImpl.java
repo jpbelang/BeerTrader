3852387fadb2f5d0e0c4f4b29cdd7abb942bc055
jpbelang <jpbelang@gmail.com>
2018-05-20 14:12:46 -0400

    Some changes to objects.

--- server/src/main/java/org/raml/jaxrs/beertrader/resources/impl/BeersImpl.java
+++ server/src/main/java/org/raml/jaxrs/beertrader/resources/impl/BeersImpl.java
@@ -14,6 +14,7 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.transaction.Transactional;
+import javax.ws.rs.WebApplicationException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,7 +36,6 @@ public class BeersImpl implements UsersUserIdBeers {
 
         List<BeerObject> beerObjects = context.createQuery("from BeerObject ", BeerObject.class).getResultList();
         List<Beer> beers = beerObjects.stream().map(BeersImpl::beerObjectToBeer).collect(Collectors.toList());
-
         return GetUsersBeersByUserIdResponse.respond200WithApplicationJson(beers);
     }
 
